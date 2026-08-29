be4d487d2e64fd553682ec9578ee7e05f319f145
Sam Fielder <sam.fielder@gmail.com>
2026-08-28 20:42:43 -0700

    Added Scrapli

--- .vscode/lab05-python/scrapli-vlans.py
+++ .vscode/lab05-python/scrapli-vlans.py
@@ -1,11 +1,10 @@
-import netmiko
-from netmiko import ConnectHandler
-from netmiko.exceptions import (
-    NetmikoTimeoutException,
-	ConnectionException,
-    NetmikoAuthenticationException,
-)
 import getpass
+from scrapli.driver.core import IOSXEDriver
+from scrapli.exceptions import (
+    ScrapliAuthenticationFailed, 
+    ScrapliTimeout, 
+    ScrapliConnectionError
+    )
 
 # define variables
 username = "admin"
@@ -13,37 +12,38 @@ password = getpass("Enter admin password: ")
 switches = ["10.10.20.173", "10.10.20.174"]
 cmds = ["vlan 10", "vlan 20", "vlan 30",]
 
-
 #connect to both switches & handle auth/ timeout exceptions - narrower than exception
 def addVlans():
 	for switch in switches:
 		device = {
 			"device_type": "cisco_ios",
 			"host": switch,
-			"username": username,
-			"password": password
+			"auth_username": username,
+			"auth_password": password,
+            "auth_strict_key": False,   # skip host-key checking
+            "transport": "system",      # or "ssh2", "paramiko", "asyncssh"
 		}
 		try:
-			with netmiko.ConnectHandler(**device) as connect:
-				output = connect.send_config_set(cmds)
-				connect.save_config()
+			with IOSXEDriver(**device) as connect:
+				output = connect.send_configs(cmds)
+				connect.send_command("write memory")
 				print(output)
 #i suspect this part wont work, connect being passed
 				result = verify(connect)
 				print(result)
-		except ConnectionException:
+		except ScrapliConnectionError:
 			print("Connection Error")
-		except NetmikoAuthenticationException:
+		except ScrapliAuthenticationFailed:
 			print("Auth Error")
-		except NetmikoTimeoutException:
+		except ScrapliTimeout:
 			print("Timeout Error")
 		
 
 #Verify with show vlan brief and return
 def verify(connect):
-	result = connect.send_command("show vlan brief")
+	result = connect.send_command("show vlan brief",use_textfsm=True)
 	print(result)
-	return result
+	return result.json()
 
 def main():
 	addVlans()
